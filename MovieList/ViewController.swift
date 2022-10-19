58a234a6b0bd93b148b765d679f28eae93f381ae
Mason <mason.rozumny23@d155.org>
2022-10-19 07:35:11 -0500

    Initial Commit

---
+++ MovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by MASON ROZUMNY on 10/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
